5226372b7ddc658754bb363c7dc581f9b52751b9
Anant Govil <44673944+a-govil@users.noreply.github.com>
2022-09-30 20:15:52 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/anantgovil/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/anantgovil/Developer/Agile_MentalHealthApp/Flutter-Login-Page-UI"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/anantgovil/Developer/Agile_MentalHealthApp/Flutter-Login-Page-UI/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/anantgovil/Developer/Agile_MentalHealthApp/Flutter-Login-Page-UI/.dart_tool/package_config.json"
