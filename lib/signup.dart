a349c47c287bffe507b534263236417cf8fd7f6c
Anant Govil <44673944+a-govil@users.noreply.github.com>
2022-10-01 01:11:23 +0530

    Evaluation 1

---
+++ lib/signup.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:day12_login/Animation/FadeAnimation.dart';
+
+
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({Key key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+      	child: Container(
+	        child: Column( 
+	          children: <Widget>[
+	            Container(
+	              height: 300,
+	              decoration: BoxDecoration(
+	                image: DecorationImage(
+	                  image: AssetImage('assets/images/background.png'),
+	                  fit: BoxFit.fill
+	                )
+	              ),
+	              child: Stack(
+	                children: <Widget>[
+	                  Positioned(
+	                    child: FadeAnimation(1.6, Container(
+	                      margin: EdgeInsets.only(top: 0),
+	                      child: Center(
+	                        child: Text("Create Account", style: TextStyle(color: Color.fromARGB(255, 227, 229, 226), fontSize: 30, fontWeight: FontWeight.bold),),
+	                      ),
+	                    )),
+	                  )
+	                ],
+	              ),
+	            ),
+	            Padding(
+	              padding: EdgeInsets.all(30.0),
+	              child: Column(
+	                children: <Widget>[
+	                  FadeAnimation(1.8, Container(
+	                    padding: EdgeInsets.all(5),
+	                    decoration: BoxDecoration(
+	                      color: Colors.white,
+	                      borderRadius: BorderRadius.circular(10),
+	                      boxShadow: [
+	                        BoxShadow(
+	                          color: Color.fromRGBO(143, 148, 251, .2),
+	                          blurRadius: 20.0,
+	                          offset: Offset(0, 10)
+	                        )
+	                      ]
+	                    ),
+	                    child: Column(
+	                      children: <Widget>[
+	                        Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          decoration: BoxDecoration(
+	                            border: Border(bottom: BorderSide(color: Colors.grey[100]))
+	                          ),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Name",
+	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                            ),
+	                          ),
+	                        ),
+	                        Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Email or Phone number",
+	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                            ),
+	                          ),
+	                        ),
+                          Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Password",
+	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                            ),
+	                          ),
+	                        ),
+                          Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Confrim Password",
+	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                            ),
+	                          ),
+	                        )
+	                      ],
+	                    ),
+	                  )),
+	                  SizedBox(height: 30,),
+	                  FadeAnimation(2, Container(
+	                    height: 50,
+	                    decoration: BoxDecoration(
+	                      borderRadius: BorderRadius.circular(10),
+	                      gradient: LinearGradient(
+	                        colors: [
+	                          Color.fromRGBO(97, 128, 106, 1),
+	                          Color.fromRGBO(97, 128, 106, .6),
+	                        ]
+	                      )
+	                    ),
+	                    child: Center(
+	                      child: Text("Sign Up", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+	                    ),
+	                  )),
+	                  SizedBox(height: 50,),
+                     TextButton(
+                      onPressed:() {Navigator.pushNamed(context, 'login');},
+                      child:const Text('Login',style: TextStyle(color: Color.fromARGB(255, 46, 53, 45), fontWeight: FontWeight.bold))),
+                      
+                    Text("Having Trouble Signing In"),
+	                ],
+	              ),
+	            )
+	          ],
+	        ),
+	      ),
+      )
+    );
+  }
+}
